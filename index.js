90a68fc7af82ccdf83e953067bcd894a509e5a92
Cody Sheridan <csheridan994@gmail.com>
2018-04-01 13:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var nombre = "Joe"
+var name = "Joe"
 var height = "74"
 
 console.log("Name:")
-console.log(nombre)
+console.log(name)
 
 console.log("Height:")
 console.log(height)
